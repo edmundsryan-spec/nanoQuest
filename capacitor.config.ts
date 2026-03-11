bd492519cb01693817d4b4d2f9dee1302c35ae6c
edmundsryan-spec <edmunds.ryan@gmail.com>
2026-03-10 23:27:53 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.nanoquest.pub',
+  appName: 'nanoQuest',
+  webDir: 'dist'
+};
+
+export default config;
